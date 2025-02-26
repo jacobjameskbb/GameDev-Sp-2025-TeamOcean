1d3f585f387146c7bf175864adc95009257c32bc
sirpineapplehead <107498118+sirpineapplehead@users.noreply.github.com>
2025-02-26 14:43:58 -0800

    made main

---
+++ Scripts/main.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+
+
+func _on_start_button_up():
+	pass # Replace with function body.
+
+
+func _on_load_button_up():
+	pass # Replace with function body.
+
+
+func _on_quit_button_up():
+	pass # Replace with function body.
