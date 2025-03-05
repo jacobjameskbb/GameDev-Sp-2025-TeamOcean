5d42468e8b2236069f6db68b7a1d1777286ae45a
sirpineapplehead <107498118+sirpineapplehead@users.noreply.github.com>
2025-03-05 13:53:59 -0800

    fixed player gravity

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
-const GRAVITY = 1
+const GRAVITY = 2
 
-const TERMINALVELOCITY = 333
+const TERMINALVELOCITY = 64
 
 const SPEED = 1
 
-var jump_speed = 1
+var jump_speed = 32
 
 var jumping = false
 
-@export var max_jump_height = 1
+@export var max_jump_height = 33
 
 var initial_jump_height: float
 
@@ -21,21 +21,25 @@ func _process(_delta):
 
 func _physics_process(delta):
 	velocity.x = 0
-	
+	print(velocity)
 	if is_on_floor():
 		if velocity.y > 0:
 			velocity.y = 0
 		
 		if Input.is_action_just_pressed(&"Jump"):
 			initial_jump_height = position.y
-			velocity += Vector2(0, -33 * jump_speed)
+			velocity += Vector2(0, -1 * jump_speed)
 			jumping = true
+	
 	else:
-		if position.y >= initial_jump_height - 66 and jumping == true:
+		if position.y <= initial_jump_height - max_jump_height and jumping == true:
+			print('ding')
 			jumping = false
 		
 		if jumping == false:
 			if velocity.y < TERMINALVELOCITY:
+				if velocity.y < 0:
+					velocity.y = 0
 				velocity.y += 1 * GRAVITY
 	
 	if Input.is_action_pressed(&"Left"):
