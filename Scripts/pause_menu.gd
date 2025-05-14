09944c2cf6c5310aeddd2917882b46ac63b3b9fe
sirpineapplehead <107498118+sirpineapplehead@users.noreply.github.com>
2025-05-14 15:18:45 -0700

    fixed bug

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -7,11 +7,5 @@ func _on_unpause_button_up():
 
 
 func _on_quit_button_up():
-	Global.load_scene(&"00000000")
-
-
-func _on_line_edit_text_submitted(new_text):
-	if StringName(new_text) in Global.item_keys.keys():
-		$LineEdit.release_focus()
-		
-		Global.give_item(StringName(new_text))
+	get_tree().paused = false
+	Global.call_deferred(&"load_scene", &"00000000")
